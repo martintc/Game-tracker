9a9dad7a14b5a322193eeaf5787b01c394d06a59
Freak <49589622+FreakHaxme@users.noreply.github.com>
2019-05-21 20:48:15 -0500

    Update GUI.java

--- src/lootTableDnD/GUI.java
+++ src/lootTableDnD/GUI.java
@@ -5,9 +5,12 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class GUI extends JFrame{
 	public GUI(){
-		
+		//A way to choose the DB
+		//Slots for the D12, all places of the 1000, 000 = 1000, button to choose, button for random rolls.
 	}
 	public GUI(boolean f){
-		
+		//A way to choose the DB
+		//Just a single button to pick random item in the list
 	}
+	
 }
