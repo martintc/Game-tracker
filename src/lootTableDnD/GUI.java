d98e433bbd3d3e90cf35996d8c7c8eda0c7d87f9
Freak <49589622+FreakHaxme@users.noreply.github.com>
2019-06-01 11:25:43 -0500

    Update GUI.java

--- src/lootTableDnD/GUI.java
+++ src/lootTableDnD/GUI.java
@@ -5,53 +5,102 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 @SuppressWarnings("serial")
 public class GUI extends JFrame{
-	private boolean type;
+	private JMenuBar menuBar;
+	private JMenu file;
+	private JMenuItem selectTable;
+	
 	private JButton randomRolls;
 	private JButton myRolls;
 	
+	private JLabel[] specialLabels;
+	private JLabel genericLabel;
+	
+	private JTextField[] fields;
+	
+	private JFileChooser chooser;
+	
+	boolean type;
+	
+	String path = "";
 	public GUI(boolean t){
 		super("Loot Table Tool");
 		setLayout(new GridBagLayout());
 		GridBagConstraints c=new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		
-		//Insert a way for them to select their loot table
-		//Use the notes organizer program for reference
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		file = new JMenu("File");
+		menuBar.add(file);
+		selectTable = new JMenuItem("Select Loot Table");
+		file.add(selectTable);
+		
+		chooser = new JFileChooser();
+		chooser.setFileFilter(new FileNameExtensionFilter("CSV Files" , "csv"));
 		
 		type = t;
-		if(t == true){
+		if(t){		//Special GUI -------------------------------------------------
 			String labelText[] = {	"<html><pre>Reward------d12:  </pre></html>",
-									"<html><pre>Hundreds----d10:  </pre></html>",
-									"<html><pre>Tens--------d10:  </pre></html>",
-									"<html><pre>Ones--------d10:  </pre></html>",
+									"<html><pre>Hundreds----d10:  </pre></html>", //
+									"<html><pre>Tens--------d10:  </pre></html>", // Might consider putting blank labels to separate these 3 from the others
+									"<html><pre>Ones--------d10:  </pre></html>", //
 									"<html><pre>Coins-------d20:  </pre></html>",
 									"<html><pre>Multiplier--d10:  </pre></html>"};
-			JLabel labels[] = new JLabel[6];
-			for(int i = 0; i<labels.length; ++i){
-				labels[i] = new JLabel(labelText[i]);
+			specialLabels = new JLabel[6];
+			for(int i = 0; i<specialLabels.length; ++i){
+				specialLabels[i] = new JLabel(labelText[i]);
 				c.gridx = 0;
 				c.gridy = i;
-				add(labels[i], c);
+				add(specialLabels[i], c);
 			}
-			JTextField fields[] = new JTextField[6];
+			fields = new JTextField[6];
 			for(int i = 0; i<fields.length; ++i){
 				fields[i] = new JTextField(5);
 				c.gridx = 1;
 				c.gridy = i;
 				add(fields[i], c);
 			}
-		}else{
-			//Probably just a single JLabel for d(table length) and a single JTextField
+			myRolls = new JButton("Get Loot");
+			c.gridx = 0;
+			c.gridy = 6;
+			c.gridwidth = 2;
+			add(myRolls, c);
+			randomRolls = new JButton("Random Loot");
+			c.gridx = 0;
+			c.gridy = 7;
+			c.gridwidth = 2;
+			add(randomRolls, c);
+		}else{		//Generic GUI -------------------------------------------------
+			genericLabel = new JLabel("Item number: ");
+			c.gridx = 0;
+			c.gridy = 0;
+			add(genericLabel, c);
+			
+			//insert a field here.
+			
+			myRolls = new JButton("Get Loot");
+			c.gridx = 0;
+			c.gridy = 1;
+			c.gridwidth = 2;
+			add(myRolls, c);
+			randomRolls = new JButton("Random Loot");
+			c.gridx = 0;
+			c.gridy = 2;
+			c.gridwidth = 2;
+			add(randomRolls, c);
 		}
-		
-		randomRolls = new JButton("Random Rolls");
-		myRolls		= new JButton("Use My Rolls");
 		
 		
 /*---------------------------------
@@ -59,13 +108,54 @@ public class GUI extends JFrame{
  */
 		EventHandler handler = new EventHandler();
 		randomRolls.addActionListener(handler);
+		myRolls.addActionListener(handler);
+		selectTable.addActionListener(handler);
 	}
 	private class EventHandler implements ActionListener{	
 		public void actionPerformed(ActionEvent event){
-			if(event.getSource()==randomRolls){
-				
-			}else if(event.getSource()==myRolls){
-				
+			if(event.getSource()==myRolls){
+				if(path == ""){
+					JOptionPane.showMessageDialog(null, "First select a loot table.");
+				}else{
+					if(type){	//Special rules
+						boolean check = true;
+						for(int i = 0; i < fields.length; ++i){
+							try{
+								Integer.parseInt(fields[i].getText());
+							}catch(Exception e){
+								JOptionPane.showMessageDialog(null, "Error: "+e);
+								check = false;
+								break;
+							}
+						}
+						if(check){
+							Loot.getItem(path,
+										 Integer.parseInt(fields[0].getText()),
+										 Integer.parseInt(fields[1].getText()),
+										 Integer.parseInt(fields[2].getText()),
+										 Integer.parseInt(fields[3].getText()),
+										 Integer.parseInt(fields[4].getText()),
+										 Integer.parseInt(fields[5].getText()));
+						}
+					}else{		//Regular rules
+						JOptionPane.showMessageDialog(null, "Not yet implemented.");
+					}
+				}
+			}else if(event.getSource()==randomRolls){
+				if(path == ""){
+					JOptionPane.showMessageDialog(null, "First select a loot table.");
+				}else{
+					if(type){	//Special rules
+						JOptionPane.showMessageDialog(null, "Not yet implemented.");
+					}else{		//Regular rules
+						JOptionPane.showMessageDialog(null, "Not yet implemented.");
+					}
+				}
+			}else if(event.getSource() == selectTable){
+				int val = chooser.showOpenDialog(null);
+				if(val == JFileChooser.APPROVE_OPTION){
+					path = chooser.getSelectedFile().getAbsolutePath();
+				}
 			}
 		}
 	}
