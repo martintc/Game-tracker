63ae1d3f66637df51ab91b8227c742a8c7cf3fd1
Freak <49589622+FreakHaxme@users.noreply.github.com>
2019-06-10 18:52:53 -0500

    Update Dice.java

--- src/dice/Dice.java
+++ src/dice/Dice.java
@@ -1,33 +1,127 @@
 package dice;
 
-public class Dice {
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+@SuppressWarnings("serial")
+public class Dice extends JFrame{
+	private JMenuBar menuBar;
+	private JMenu file;
+	private JMenuItem load;
+	
+	private JLabel commonLabel;
+	private JButton[] commonButtons;
+	private String[] commonNames = {"d4","d6","d8","d10","d12","d20"};
+	
+	private JButton myCombos;
+
+	private String path = "";
+	public Dice(){
+		super("Dice Manager");
+		setLayout(new GridBagLayout());
+		GridBagConstraints c=new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+/*----------------------------------
+ * 			Menu Bar
+ */
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		file = new JMenu("File");
+		menuBar.add(file);
+		load = new JMenuItem("Load");
+		file.add(load);
+		
+/*----------------------------------
+ * 			Other
+ */
+		//Add all the Insets for spacing
+		
+		commonLabel = new JLabel("Common: ");
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		add(commonLabel, c);
+		
+		commonButtons = new JButton[6];
+		c.gridy = 1;
+		for(int i = 0; i<(int)(commonButtons.length/2); ++i){
+			commonButtons[i] = new JButton(commonNames[i]);
+			add(commonButtons[i], c);
+			c.gridx++;
+		}
+		c.gridy = 2;
+		c.gridx = 0;
+		for(int i = (int)(commonButtons.length/2); i<commonButtons.length; ++i){
+			commonButtons[i] = new JButton(commonNames[i]);
+			add(commonButtons[i], c);
+			c.gridx++;
+		}
+		
+		myCombos = new JButton("My Combos"); //add to its own panel? I don't want it to affect the size of the other buttons.
+		c.gridy = 3;
+		c.gridx = 0;
+		add(myCombos, c);
+/*----------------------------------
+ * 			Events
+ */
+		EventHandler handler = new EventHandler();
+		load.addActionListener(handler);
+		for(int i = 0; i<commonButtons.length; ++i){
+			commonButtons[i].addActionListener(handler);
+		}
+		myCombos.addActionListener(handler);
+		
+	}
+	private class EventHandler implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			if(event.getSource()==load){	
+				
+			}else if(event.getSource()==myCombos){
+				
+			}else if(event.getSource()==commonButtons[0]){
+				
+			}else if(event.getSource()==commonButtons[1]){
+				
+			}else if(event.getSource()==commonButtons[2]){
+				
+			}else if(event.getSource()==commonButtons[3]){
+				
+			}else if(event.getSource()==commonButtons[4]){
+				
+			}else if(event.getSource()==commonButtons[5]){
+				
+			}
+		}
+	}
+	
 	public static void run(){
-		dice.GUI myInterface = new dice.GUI();
+		Dice myInterface = new Dice();
 		myInterface.pack();
 		myInterface.setVisible(true);
 		myInterface.setResizable(true);
+		
+		//attempt to load a default database
 	}
 }
 
 /*
 
-Okay, so what is even the vision here?
---------------------------------------
-* Individual combo objects that store the info. (2d6+1 is multi=2, d=6, c=1)
-
-* GUI should have a "add", "remove", and "load"
-	- "load"	loads a file containing all saved combos. Needs to be able to create new files, OR separate button?
-	
-	- "add"		adds a combo.
-	- "remove"	removes a combo.
-		* both should require you to load a DB first
-		* Probably not a JMenu for these two?
-
 * Need to be able to name them/rename them.
 * SUGGESTED that they include a header? Like "Goblin: Punch"
 	- Display them alphabetically
-	
-	
-* Dice.run() should attempt to load a default DB if there is one in the same directory.
+
+* I want to be able to graph the probability distribution of a dice combination.
+
+* Each combo should be stored in the DB in 1 line. Just use different delimiters. Commas separate items within a combo, semicolons separate combos.
 
  */
