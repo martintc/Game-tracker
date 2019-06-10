2df1ecfdbe115ba329a97686ff63e02dd219f6f8
Freak <49589622+FreakHaxme@users.noreply.github.com>
2019-06-10 18:55:29 -0500

    Create CreatePopup.java

---
+++ src/dice/CreatePopup.java
@@ -0,0 +1,22 @@
+package dice;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class CreatePopup extends JFrame{
+	public CreatePopup(){
+		
+		
+/*----------------------------------
+ * 			Events
+ */
+	EventHandler handler = new EventHandler();
+	}
+	private class EventHandler implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			
+		}
+	}
+}
