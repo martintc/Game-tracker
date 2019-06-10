fb25c1fac3d6331b8d131a224294625a831e2eb3
Freak <49589622+FreakHaxme@users.noreply.github.com>
2019-06-10 16:58:37 -0500

    Create Combo.java

---
+++ src/dice/Combo.java
@@ -0,0 +1,23 @@
+package dice;
+
+public class Combo {
+	private int multi;
+	private int d;
+	private int c;
+	
+	public Combo(){
+		
+	}
+/*----------------------------------
+ * 		  		Getters
+ */
+	public int getRoll(){
+		return (multi*d) + c;
+	}
+	
+/*----------------------------------
+ * 		  		Setters
+ */
+
+	
+}
